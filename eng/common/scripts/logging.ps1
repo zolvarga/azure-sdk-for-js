102d9b347e187a686f0050076d81ffa4c0da04ac
Chidozie Ononiwu <chononiw@microsoft.com>
2021-02-01 12:27:14 -0800

    Update collect changelog to use full date

--- eng/common/scripts/logging.ps1
+++ eng/common/scripts/logging.ps1
@@ -1,40 +1,37 @@
-if (-not $isDevOpsRun)
-{
-  $isDevOpsRun = ($null -ne $env:SYSTEM_TEAMPROJECTID)
-}
+$isDevOpsRun = ($null -ne $env:SYSTEM_TEAMPROJECTID)
 
 function LogWarning
 {
-    if ($isDevOpsRun) 
-    {
-        Write-Host "##vso[task.LogIssue type=warning;]$args"
-    }
-    else
-    {
-        Write-Warning "$args"
-    }
+  if ($isDevOpsRun) 
+  {
+    Write-Host "##vso[task.LogIssue type=warning;]$args"
+  }
+  else
+  {
+    Write-Warning "$args"
+  }
 }
 
 function LogError
 {
-    if ($isDevOpsRun) 
-    {
-        Write-Host "##vso[task.LogIssue type=error;]$args"
-    }
-    else 
-    {
-        Write-Error "$args"
-    }
+  if ($isDevOpsRun) 
+  {
+    Write-Host "##vso[task.LogIssue type=error;]$args"
+  }
+  else 
+  {
+    Write-Error "$args"
+  }
 }
 
 function LogDebug
 {
-    if ($isDevOpsRun) 
-    {
-        Write-Host "[debug]$args"
-    }
-    else 
-    {
-        Write-Debug "$args"
-    }
+  if ($isDevOpsRun) 
+  {
+    Write-Host "[debug]$args"
+  }
+  else 
+  {
+    Write-Debug "$args"
+  }
 }
